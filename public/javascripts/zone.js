5021e7a634c9f6f5ee70ed741989b18997209859
Anthony Guidarelli <anthony.guidarelli@gmail.com>
2011-08-20 14:12:34 -0400

    Fix zone.js for newest spree

--- public/javascripts/zone.js
+++ public/javascripts/zone.js
@@ -1,64 +1,61 @@
-$j(function() {
+$j(function() { 
   if ($j('#country_based').attr('checked')) {
     show_country();
   } else if ($j('#state_based').attr('checked')) {
     show_state();
-  } else if ($j('#zip_code_range_based').attr('checked')) {
+	} else if ($j('#zip_code_range_based').attr('checked')) {
     show_zip_code_range();
-  } else {
+  } else {        
     show_zone();
   }
-  $j('#country_based').click(function()   { show_country();} );
-  $j('#state_based').click(function()     { show_state();} );
-  $j('#zip_code_range_based').click(function()  { show_zip_code_range();} );
-  $j('#zone_based').click(function()      { show_zone();} );
-})
-
+  $j('#country_based').click(function() { show_country();} );
+  $j('#state_based').click(function() { show_state();} );
+	$j('#zip_code_range_based').click(function()  { show_zip_code_range();} );
+  $j('#zone_based').click(function() { show_zone();} ); 
+})   
+                                                        
 var show_country = function() {
-  $j('#state_members :input').each(function() { $(this).disable(); })
+  $j('#state_members :input').each(function() { $(this).attr('disabled', 'disabled'); })
   $j('#state_members').hide();
-  $j('#zone_members :input').each(function() { $(this).disable(); })
+  $j('#zone_members :input').each(function() { $(this).attr('disabled', 'disabled'); })
   $j('#zone_members').hide();
-  $j('#zip_code_range_members :input').each(function() { $(this).disable(); })
+	$j('#zip_code_range_members :input').each(function() { $(this).attr('disabled', 'disabled'); })
   $j('#zip_code_range_members').hide();
-  // Show Country
-  $j('#country_members :input').each(function() { $(this).enable(); })
+  $j('#country_members :input').each(function() { $(this).removeAttr('disabled'); })
   $j('#country_members').show();
 };
 
 var show_state = function() {
-  $j('#country_members :input').each(function() { $(this).disable(); })
+  $j('#country_members :input').each(function() { $(this).attr('disabled', 'disabled'); })
   $j('#country_members').hide();
-  $j('#zone_members :input').each(function() { $(this).disable(); })
+  $j('#zone_members :input').each(function() { $(this).attr('disabled', 'disabled'); })
   $j('#zone_members').hide();
-  $j('#zip_code_range_members :input').each(function() { $(this).disable(); })
+	$j('#zip_code_range_members :input').each(function() { $(this).attr('disabled', 'disabled'); })
   $j('#zip_code_range_members').hide();
-  // Show State
-  $j('#state_members :input').each(function() { $(this).enable(); })
+  $j('#state_members :input').each(function() { $(this).removeAttr('disabled'); })
   $j('#state_members').show();
 };
 
 var show_zip_code_range = function() {
-  $j('#country_members :input').each(function() { $(this).disable(); })
-  $j('#country_members').hide();
-  $j('#zone_members :input').each(function() { $(this).disable(); })
-  $j('#zone_members').hide();
-  $j('#state_members :input').each(function() { $(this).disable(); })
+  $j('#state_members :input').each(function() { $(this).attr('disabled', 'disabled'); })
   $j('#state_members').hide();
-  // Show ZIP code
-  $j('#zip_code_range_members :input').each(function() { $(this).enable(); })
+  $j('#country_members :input').each(function() { $(this).attr('disabled', 'disabled'); })
+  $j('#country_members').hide();
+  $j('#zip_code_range_members :input').each(function() { $(this).removeAttr('disabled'); })
   $j('#zip_code_range_members').show();
+  $j('#zone_members :input').each(function() { $(this).attr('disabled', 'disabled'); })
+  $j('#zone_members').hide();
 };
 
 var show_zone = function() {
-  $j('#state_members :input').each(function() { $(this).disable(); })
+  $j('#state_members :input').each(function() { $(this).attr('disabled', 'disabled'); })
   $j('#state_members').hide();
-  $j('#country_members :input').each(function() { $(this).disable(); })
+  $j('#country_members :input').each(function() { $(this).attr('disabled', 'disabled'); })
   $j('#country_members').hide();
-  $j('#zip_code_range_members :input').each(function() { $(this).disable(); })
+	$j('#zip_code_range_members :input').each(function() { $(this).attr('disabled', 'disabled'); })
   $j('#zip_code_range_members').hide();
-  // Show Zone
-  $j('#zone_members :input').each(function() { $(this).enable(); })
+  $j('#zone_members :input').each(function() { $(this).removeAttr('disabled'); })
   $j('#zone_members').show();
 };
+
 
